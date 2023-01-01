042e52e90312e0a309b5d6ed433fcb32de5b3156
China-7 <115672054+China-7@users.noreply.github.com>
2023-01-01 13:31:45 -0400

    Add files via upload
    Asi es que voy en python

---
+++ practicas python.py
@@ -0,0 +1,125 @@
+"""
+
+a = input(" VALOR DE A :    ")
+b= input(" VALOR DE B :    ")
+
+a , b =  b , a
+
+print("El valor de a es:  ",a)
+print("El valor de b es:  ",b)
+"""
+"""
+import math
+
+radio = float(input("Radio :   "))
+
+area = math.pi * radio**2
+
+longitud  = 2 * math.pi * radio
+
+print("El area es  : ",area)
+
+print("La longitud 'es:  ", longitud)
+
+              
+"""
+
+"""
+
+precio = float(input("Digite el precio :  "))
+descuento = precio * 0.15
+
+precio_final = precio - descuento
+
+print("El precio a pagar es:  ",precio_final )
+
+
+
+"""
+"""
+edad = int(input("Digite su edad:  "))
+
+
+if edad >0:
+      print("la edad es correcta")
+      if edad >= 18:
+            print("Es mayor de edad")
+
+else:
+      print("No es mayor de edad")
+
+"""
+
+
+
+
+
+
+"""
+
+# Los numeros pares
+
+
+num1 = int(input("Ingresa un numero par : "))
+num2 = int(input("Ingresa un numero par : "))
+
+if num1%2==0  and num2%2==0:
+      print ("Los numeros son pares")
+
+
+
+
+elif num1%1==0 and num2%2 !=0:
+      print(num1,"Es par")
+
+
+
+elif num1 % 2 != 0  and num2 % 2==0:
+      print(num2 , "Es un numero par")
+
+
+
+else:
+      print("Ambos numeros son impares")
+"""
+
+
+
+pila =[1,2,3]
+print(pila)
+
+
+# Agregando elementos al final de la pila
+"""
+pila.append(4)
+
+print(pila)
+
+"""
+
+
+
+#Funciones
+
+def saludar():
+      print("Hola Mundo")
+
+saludar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
